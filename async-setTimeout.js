916750eddba1ef1702142f216132276fa09f3dcf
晓荷 <xhlsrj@qq.com>
2018-03-23 15:14:48 +0800

    Update async-setTimeout.js

--- async-setTimeout.js
+++ async-setTimeout.js
@@ -1,10 +1,11 @@
-async function fn(resolve = () => { }) {
+async function fn(resolve = data => data) {
+    let data = {};
     if (conditions) {
-        return resolve();
+        return resolve(data);
     }
     let p = new Promise((resolve) => {
         setTimeout(fn, 1000, resolve);
     });
-    await p.then(() => { });
-    resolve();
+    data = await p.then(data => data);
+    return resolve(data);
 }
