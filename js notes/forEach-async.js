f49e633ef05553afd5d7892ad061e7e439823cd1
晓荷 <xhlsrj@qq.com>
2019-04-08 18:08:24 +0800

    Update forEach-async.js

--- js notes/forEach-async.js
+++ js notes/forEach-async.js
@@ -13,3 +13,20 @@ arr.forEach(async (e) => {
 // 3000
 
 // use for
+async function fn() {
+  for (let i = 0; i < arr.length; i++) {
+    let e = arr[i];
+    let p = new Promise((resolve) => {
+      setTimeout(resolve, e, e);
+    });
+    console.log(`as you see`);
+    await p.then(console.log);
+  }
+}
+
+fn();
+
+// as you see
+// 3000
+// as you see
+// 1000
