cdc59231089b1ace630780ca80bec65912c9d7db
晓荷 <xhlsrj@qq.com>
2025-04-10 14:31:56 +0800

    Create number.js

---
+++ js notes/number.js
@@ -0,0 +1,9 @@
+// As we know:
+
+// 0.30000000000000004
+console.log(0.1 + 0.2);
+// 7.000000000000001
+console.log(0.07 * 100);
+
+// A simple solution for certain precision
+console.log(Number(0.999.toFixed(2)));
